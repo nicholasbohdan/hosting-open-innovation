854264e1ae09bea70b666fc67e2e7186f4e467d8
N B <Nicholas.bohdan>
2021-05-10 09:34:57 +0700

    case sensitive

--- pages/idea_catalog/home/index.js
+++ pages/idea_catalog/home/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../../../styles/IdeaCatalog.module.css';
 import Header from '../../../components/header';
 import FilterByTopic from '../../../components/filterbytopic';
-import Footer from '../../../components/footer';
+import Footer from '../../../components/Footer';
 import { Row } from 'reactstrap';
 import IdeasCard from 'components/shared/IdeasCard';
 import BaseLayout from 'components/layouts/BaseLayout';
